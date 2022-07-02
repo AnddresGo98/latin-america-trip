75ff20a4bea6bd474fc922c68d4eb4aff7a05952
Andrés González <84155091+AnddresGo98@users.noreply.github.com>
2022-07-02 12:07:55 -0500

    add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'prueba',
-    data:{animation: 'prueba'}
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('graph') graph: ElementRef | undefined;
+
+  viewBox: string = '0 0 1200 100';
+  widthGraph: number = 1200;
+  heightGraph: number = 100;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('estamos en footer');
+    let hola = Math.round(Math.random() * 100);
+   /*  setInterval(() => {
+      console.log(hola++);
+    },1000) */
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.graph);
+    //this.setGraphSizes();
+  }
+
+  setGraphSizes(): void {
+    this.widthGraph = this.graph?.nativeElement.offsetWidth;
+    this.heightGraph = this.graph?.nativeElement.offsetHeight;
+    this.viewBox = `0 0 ${this.widthGraph} ${this.heightGraph}`;
+  }
+
+  onResizeGraph(s: any): void {
+    console.log('redimencion');
   }
 
 }
